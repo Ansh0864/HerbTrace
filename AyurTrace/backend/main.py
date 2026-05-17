c42ed93abdc5ec8a75eed51548d0e3a3d2639ba5
Shantanu Chauhan <iamansh86@gmail.com>
2026-05-17 13:05:50 +0530

    fixedd

--- AyurTrace/backend/main.py
+++ AyurTrace/backend/main.py
@@ -73,10 +73,9 @@ except FileNotFoundError:
 app = FastAPI()
 
 origins = ["http://localhost", "http://localhost:5173", "http://127.0.0.1:5173"]
-#  UPDATED BLOCK
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows your live Render frontend domain to communicate smoothly
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
